bf44c154dd52e8ae7d69a7dea69c549a1fc39468
LightQuantum <self@lightquantum.me>
2022-08-24 19:21:42 +0800

    feat(mbp): git

--- mbp/home.nix
+++ mbp/home.nix
@@ -8,5 +8,24 @@
   };
   programs = {
     home-manager.enable = true;
+    lazygit.enable = true;
+    git = {
+      enable = true;
+      lfs.enable = true;
+      userName = "LightQuantum";
+      userEmail = "self@lightquantum.me";
+      signing = {
+        key = "A99DCF320110092028ECAC42E53ED56B7F20B7BB";
+        signByDefault = true;
+      };
+      ignores = [
+        "/.idea"
+      ];
+      extraConfig = {
+        pull.ff = "only";
+        push.autoSetupRemote = true;
+        absorb.maxStack = 50;
+      };
+    };
   };
 }

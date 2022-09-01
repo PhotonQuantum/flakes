e836ce33201f91b023940027fba9e49072150b67
LightQuantum <self@lightquantum.me>
2022-09-01 16:19:04 +0800

    feat(meow): synapse

--- meow/configuration.nix
+++ meow/configuration.nix
@@ -11,6 +11,21 @@
       ./hardware-configuration.nix
     ];
 
+  deployment.keys = {
+    "homeserver.yaml" = {
+      keyFile = ../secrets/homeserver.yaml;
+      destDir = "/var/keys";
+      user = "matrix-synapse";
+      group = "matrix-synapse";
+    };
+    "homeserver.signing.key" = {
+      keyFile = ../secrets/lightquantum.me.signing.key;
+      destDir = "/var/lib/matrix-synapse";
+      user = "matrix-synapse";
+      group = "matrix-synapse";
+    };
+  };
+
   nix.package = pkgs.nix;
   nix.gc.automatic = true;
   nix.settings = {
@@ -52,6 +67,7 @@
     extraGroups = [ "wheel" "docker" ]; # Enable ‘sudo’ for the user.
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCu4c/RtyBgjlkM32w9qeXL/5G1xN5LbUu4M8yMXT75ZdRV5eTf9w5rgnFvf1jWmM8J97hWfqxUqCzZbPz2fB56S0r4MoA12NZduNUPbUjIJGAhf/r/aNFgVSTwep+V2OL+MZjyOi1hjklwHBpqjjz9DnSOoH+8ZhEi0U3axCC5qlKs6BI2aamiV3GMifAHXaHy8IOFWkcD+qWG6bRtfRrGpXuuB6WOUCdv1IFsiqJWnJcw2JwfgS8HAKvTZI/GuYQUJ+IbwPy7iIz7WaGXy0w9hOhTO2Bhcixf7u1uXAB86J8279IcFJ9tXhyRrE9/trX5FqIJkr/OcMUHCBS2O8uvtlhxVhegFsbo2Cg2xfbW3ri0dFm9jV0T+0JOKhLEX9trxxr2gR/uUYDyZnZ4UI6eKGKIhZOnI6B0TeKuW6ojxxgvuZJ6q4kpOSvKc7oKFM/f8w5eeqRwos6/J/K+DPIelvIMw9dPO0U/bWm8oi7d/c86q2K67MdKUpdPOVcCM0zp4KFdDl0UnLOkHMM/tgPLFwab4bmhUq57ryB5gJEW1MLA4CCQEHayGObIlzk64TZF9iUUVVPL5HsuarmDWjT3HSwyAGjp8CkfL7EVk8kWD3CiRd0VTu6qWaxTS6vbeThuGPRoyre6SPmiVWcMHJHxX+djQ305bYYsbovupVAE4Q=="
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFSZqvCJyC4hCGORRSKxWFRg8M/LK+hUebAQ8wx2xPxW lightquantum@vultr"
     ];
     home = "/home/lightquantum";
     shell = pkgs.zsh;
@@ -79,7 +95,7 @@
     curl
     wget
     lazydocker
-    netdata   # This is necessary to bring netdata-claim.sh to default shell.
+    netdata # This is necessary to bring netdata-claim.sh to default shell.
   ];
 
   virtualisation.docker.enable = true;
@@ -153,8 +169,81 @@
     };
   };
 
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_14;
+    initialScript = pkgs.writeText "synapse-init.sql" ''
+      CREATE USER "matrix-synapse";
+      CREATE DATABASE "synapse" WITH OWNER "matrix-synapse"
+        TEMPLATE template0
+        LC_COLLATE = "C"
+        LC_CTYPE = "C";
+    '';
+  };
+
+  services.matrix-synapse = {
+    enable = true;
+    withJemalloc = true;
+    settings = {
+      server_name = "lightquantum.me";
+      public_baseurl = "https://chat.lightquantum.me";
+      listeners = [
+        {
+          port = 8080;
+          bind_addresses = [ "0.0.0.0" ];
+          type = "http";
+          tls = false;
+          x_forwarded = true;
+          resources = [{
+            names = [ "client" "federation" ];
+            compress = false;
+          }];
+        }
+      ];
+      database = {
+        name = "psycopg2";
+        txn_limit = 10000;
+        args = {
+          user = "matrix-synapse";
+          database = "synapse";
+          host = "/var/run/postgresql/";
+          port = 5432;
+          cp_min = 5;
+          cp_max = 10;
+        };
+      };
+      url_preview_enabled = true;
+      url_preview_ip_range_blacklist = [
+        "127.0.0.0/8"
+        "10.0.0.0/8"
+        "172.16.0.0/12"
+        "192.168.0.0/16"
+        "100.64.0.0/10"
+        "192.0.0.0/24"
+        "169.254.0.0/16"
+        "192.88.99.0/24"
+        "198.18.0.0/15"
+        "192.0.2.0/24"
+        "198.51.100.0/24"
+        "203.0.113.0/24"
+        "224.0.0.0/4"
+        "::1/128"
+        "fe80::/10"
+        "fc00::/7"
+        "2001:db8::/32"
+        "ff00::/8"
+        "fec0::/10"
+      ];
+      max_spider_size = "10M";
+      enable_registration = true;
+      registrations_require_3pid = [ "email" ];
+      registration_requires_token = true;
+    };
+    extraConfigFiles = [ "/var/keys/homeserver.yaml" ];
+  };
+
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ ];
+  networking.firewall.allowedTCPPorts = [ 8080 ];
   networking.firewall.allowedUDPPorts = [ 20423 ];
 
   # Copy the NixOS configuration file and link it from the resulting system

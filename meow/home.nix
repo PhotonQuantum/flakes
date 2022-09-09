054046c8e3b5924de14b7ce81edd7d14c4684f29
LightQuantum <self@lightquantum.me>
2022-09-09 13:50:09 +0800

    chore(meow): remove redundant code

--- meow/home.nix
+++ meow/home.nix
@@ -25,38 +25,5 @@
     };
     starship.enable = true;
     lazygit.enable = true;
-    nixvim = {      enable = true;
-      options = {
-        number = true;
-        relativenumber = true;
-        clipboard = "unnamedplus";
-      };
-      plugins = {
-        nix.enable = true;
-      };
-      extraPlugins = with pkgs.vimPlugins; [
-        quick-scope
-        suda-vim
-      ];
-      maps = {
-        normal = {
-          "H" = { action = "^"; };
-          "L" = { action = "$"; };
-          "ZA" = { action = ":w suda://%<Return>:q<Return>"; };
-        };
-        visual = {
-          "H" = { action = "^"; };
-          "L" = { action = "$"; };
-        };
-        insert = {
-          "<S-CR>" = { action = "<Esc>"; };
-        };
-        command = {
-          "e!!" = { action = "e suda://%"; };
-          "r!!" = { action = "e suda://%"; };
-          "w!!" = { action = "w suda://%"; };
-        };
-      };
-    };
   };
 }

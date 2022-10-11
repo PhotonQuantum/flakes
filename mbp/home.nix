22a3de1d492c71b79283321b471996dc07791028
LightQuantum <self@lightquantum.me>
2022-10-12 00:30:30 +0800

    feat(mbp/home): up/down for in context history search

--- mbp/home.nix
+++ mbp/home.nix
@@ -152,6 +152,12 @@
         }
         autoload -U select-word-style
         select-word-style bash
+        autoload -U up-line-or-beginning-search
+        autoload -U down-line-or-beginning-search
+        zle -N up-line-or-beginning-search
+        zle -N down-line-or-beginning-search
+        bindkey "$terminfo[kcuu1]" up-line-or-beginning-search # Up
+        bindkey "$terminfo[kcud1]" down-line-or-beginning-search # Down
       '';
       envExtra = ". $HOME/.cargo/env";
     };

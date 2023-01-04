6e2cce56191cb2c51dec5b755d98b73001fb7fe9
LightQuantum <self@lightquantum.me>
2023-01-04 23:57:24 +0800

    feat(meow): borg compact

---
+++ meow/borg.nix
@@ -0,0 +1,77 @@
+{ config, pkgs, lib, ... }:
+
+{
+  deployment.keys = {
+    "id_ed25519_borg" = {
+      keyFile = ../secrets/id_ed25519_meow_borg;
+      destDir = "/var/keys";
+      user = "root";
+      group = "root";
+    };
+    "id_ed25519_borg.pub" = {
+      keyFile = ../secrets/id_ed25519_meow_borg.pub;
+      destDir = "/var/keys";
+      user = "root";
+      group = "root";
+    };
+  };
+
+  services.borgbackup.jobs = {
+    synapse = {
+      paths = [ "/var/backup/postgresql" "/var/lib/matrix-synapse" ];
+      repo = "ssh://c96qu46z@c96qu46z.repo.borgbase.com/./repo";
+      encryption.mode = "none";
+      compression = "auto,lz4";
+      startAt = "*-*-* 00:30:00";
+      environment = {
+        BORG_RSH = "ssh -i /var/keys/id_ed25519_borg";
+      };
+    };
+  };
+
+  services.borgbackup.repos = {
+    aliyun = {
+      path = "/var/lib/borgbackup/aliyun";
+      quota = "30G";
+      user = "borg-aliyun";
+      authorizedKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOvwlbYZzoGg+MYu9HyXhTor07AyoAEbRKpUpNi15LVu"
+      ];
+    };
+    hydev = {
+      path = "/var/lib/borgbackup/hydev";
+      quota = "10G";
+      user = "borg-hydev";
+      authorizedKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHmsSyeENaYXUWWXDIETEu1u8Ah7zEX8dCcXuGcqWLxQ"
+      ];
+    };
+    projects = {
+      path = "/var/lib/borgbackup/projects";
+      quota = "500G";
+      user = "borg-projects";
+      authorizedKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL3wabckxXT3q9ih7Y070OKjI3lf3+VuLrfilj3FzpK8"
+      ];
+    };
+  };
+
+  systemd.services = lib.mapAttrs'
+    (repo: repoCfg: {
+      name = "borgbackup-compact-${repo}";
+      value = {
+        path = with pkgs; [ borgbackup ];
+        script = "borg compact --verbose ${repoCfg.path}";
+        serviceConfig = {
+          CPUSchedulingPolicy = "idle";
+          IOSchedulingClass = "idle";
+          PrivateTmp = true;
+          ProtectSystem = "strict";
+          ReadWritePaths = repoCfg.path;
+          User = repoCfg.user;
+        };
+        startAt = "weekly";
+      };
+    })
+    config.services.borgbackup.repos;
+}
---
+++ meow/matrix.nix
@@ -0,0 +1,108 @@
+{ pkgs, ... }:
+{
+  deployment.keys = {
+    "homeserver.yaml" = {
+      keyFile = ../secrets/homeserver.yaml;
+      destDir = "/var/keys";
+      user = "matrix-synapse";
+      group = "matrix-synapse";
+    };
+    "homeserver.signing.key" = {
+      keyFile = ../secrets/lightquantum.me.signing.key;
+      destDir = "/var/lib/matrix-synapse";
+      user = "matrix-synapse";
+      group = "matrix-synapse";
+    };
+  };
+
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_14;
+    initialScript = pkgs.writeText "synapse-init.sql" ''
+      CREATE USER "matrix-synapse";
+      CREATE DATABASE "synapse" WITH OWNER "matrix-synapse"
+        TEMPLATE template0
+        LC_COLLATE = "C"
+        LC_CTYPE = "C";
+    '';
+    ensureUsers = [
+      {
+        name = "root";
+        ensurePermissions = {
+          "ALL TABLES IN SCHEMA public" = "ALL PRIVILEGES";
+        };
+      }
+    ];
+  };
+
+  services.postgresqlBackup = {
+    enable = true;
+    startAt = "*-*-* 00:15:00";
+    databases = [ "synapse" ];
+  };
+
+  services.matrix-synapse = {
+    enable = true;
+    withJemalloc = true;
+    settings = {
+      server_name = "lightquantum.me";
+      public_baseurl = "https://chat.lightquantum.me";
+      listeners = [
+        {
+          port = 8080;
+          bind_addresses = [ "0.0.0.0" ];
+          type = "http";
+          tls = false;
+          x_forwarded = true;
+          resources = [{
+            names = [ "client" "federation" ];
+            compress = false;
+          }];
+        }
+      ];
+      database = {
+        name = "psycopg2";
+        txn_limit = 10000;
+        args = {
+          user = "matrix-synapse";
+          database = "synapse";
+          host = "/var/run/postgresql/";
+          port = 5432;
+          cp_min = 5;
+          cp_max = 10;
+        };
+      };
+      url_preview_enabled = true;
+      url_preview_ip_range_blacklist = [
+        "127.0.0.0/8"
+        "10.0.0.0/8"
+        "172.16.0.0/12"
+        "192.168.0.0/16"
+        "100.64.0.0/10"
+        "192.0.0.0/24"
+        "169.254.0.0/16"
+        "192.88.99.0/24"
+        "198.18.0.0/15"
+        "192.0.2.0/24"
+        "198.51.100.0/24"
+        "203.0.113.0/24"
+        "224.0.0.0/4"
+        "::1/128"
+        "fe80::/10"
+        "fc00::/7"
+        "2001:db8::/32"
+        "ff00::/8"
+        "fec0::/10"
+      ];
+      max_spider_size = "10M";
+      enable_registration = true;
+      registrations_require_3pid = [ "email" ];
+      registration_requires_token = true;
+    };
+    extraConfigFiles = [ "/var/keys/homeserver.yaml" ];
+  };
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [ 8080 ];
+  networking.firewall.allowedUDPPorts = [ 20423 ];
+}

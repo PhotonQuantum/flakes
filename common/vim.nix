03e8e27e10afd5ccf9e8a3ae76cf010966644c2b
LightQuantum <self@lightquantum.me>
2023-08-30 00:26:02 +0800

    feat(vim): disable rust-tools

--- common/vim.nix
+++ common/vim.nix
@@ -15,19 +15,19 @@
         hlsearch = true;
       };
       extraConfigLua = ''
-        local rt = require("rust-tools")
+        -- local rt = require("rust-tools")
         require("which-key").setup { }
 
-        rt.setup({
-          server = {
-            on_attach = function(_, bufnr)
-              -- Hover actions
-              vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-              -- Code action groups
-              vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-            end,
-          },
-        })
+        -- rt.setup({
+        --   server = {
+        --     on_attach = function(_, bufnr)
+        --       -- Hover actions
+        --       vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+        --       -- Code action groups
+        --       vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+        --     end,
+        --   },
+        -- })
         require('gitsigns').setup()
 
         require("toggleterm").setup {
@@ -164,7 +164,7 @@
         telescope-file-browser-nvim
         auto-session
         auto-save-nvim
-        rust-tools-nvim
+        # rust-tools-nvim
         gitsigns-nvim
         dressing-nvim
         lualine-lsp-progress

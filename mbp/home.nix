6d6da6bcb855ea0ba6a0907a5ec94860a07bc40e
LightQuantum <self@lightquantum.me>
2024-03-20 02:22:14 +0800

    fix: prio nix path over system in fish

--- mbp/home.nix
+++ mbp/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, system, yazi, ... }:
+{ pkgs, lib, system, osConfig, yazi, ... }:
 
 {
   imports = [
@@ -319,6 +319,18 @@
         fish_add_path --prepend --global ~/.nargo/bin
         set fish_escape_delay_ms 300
       '' + (builtins.readFile ./wezterm.fish);
+      loginShellInit =
+        let
+          # This naive quoting is good enough in this case. There shouldn't be any
+          # double quotes in the input string, and it needs to be double quoted in case
+          # it contains a space (which is unlikely!)
+          dquote = str: "\"" + str + "\"";
+
+          makeBinPathList = map (path: path + "/bin");
+        in ''
+          fish_add_path --move --prepend --path ${lib.concatMapStringsSep " " dquote (makeBinPathList osConfig.environment.profiles)}
+          set fish_user_paths $fish_user_paths
+        '';
       plugins = [
         {
           name = "Done";

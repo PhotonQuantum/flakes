cc67bc1b8ba6480a48bebffecfed59dc0e26b5f3
LightQuantum <self@lightquantum.me>
2024-12-09 00:30:48 -0500

    feat(mbp): opam

--- mbp/home.nix
+++ mbp/home.nix
@@ -353,6 +353,7 @@ in
         set fish_escape_delay_ms 300
         builtin functions -e fish_mode_prompt
         eval (${pkgs.starship}/bin/starship init fish)
+        test -r ~/.opam/opam-init/init.fish && source ~/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
       '' + builtins.readFile ./wezterm.fish;
       loginShellInit =
         let

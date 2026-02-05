fbca0e20db8b9441263df2eac9575452d4d30484
LightQuantum <self@lightquantum.me>
2026-02-05 16:30:08 -0500

    fix: yazi smart-enter

--- common/yazi/smart-enter.yazi/main.lua
+++ common/yazi/smart-enter.yazi/main.lua
@@ -2,7 +2,7 @@
 
 local function entry(self)
 	local h = cx.active.current.hovered
-	ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = true, interactive = true })
+	ya.emit(h and h.cha.is_dir and "enter" or "open", { hovered = true, interactive = true })
 end
 
 return { entry = entry }

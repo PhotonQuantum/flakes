32d58f7fe83a0c7721ca2fb994ad0e6fb5227830
LightQuantum <self@lightquantum.me>
2026-02-05 16:44:54 -0500

    chore: remove redundant aerospace launchctl file

--- mbp/aerospace/config.nix
+++ mbp/aerospace/config.nix
@@ -3,13 +3,6 @@
   # 1. using hm to manage the config because we don't want to restart the service every time we change the config
   # 2. not using services.aerospace because we want to override default config path
   environment.systemPackages = [ pkgs.aerospace ];
-  launchd.user.agents.aerospace = {
-    command = "${pkgs.aerospace}/Applications/AeroSpace.app/Contents/MacOS/AeroSpace";
-    serviceConfig = {
-      KeepAlive = true;
-      RunAtLoad = true;
-    };
-  };
 
   system.defaults = {
     dock.expose-group-apps = true;

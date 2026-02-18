7ff8e9c0952d9fc66395e4a8095ceab23d7eb5c3
LightQuantum <self@lightquantum.me>
2026-02-17 23:30:37 -0500

    homelab: advanced network

---
+++ hosts/homelab/microvms/vms/static-http.nix
@@ -0,0 +1,29 @@
+{
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    virtualHosts."static-http.local" = {
+      default = true;
+      locations."/" = {
+        root = "/etc/nginx/static";
+        index = "index.html";
+      };
+      locations."/internet/" = {
+        extraConfig = ''
+          resolver 1.1.1.1 8.8.8.8 ipv6=off;
+          set $internet_upstream "http://example.com";
+          proxy_set_header Host example.com;
+          proxy_pass $internet_upstream;
+        '';
+      };
+    };
+  };
+
+  environment.etc."nginx/static/index.html".text = ''
+    <!doctype html>
+    <html>
+      <head><title>homelab microvm</title></head>
+      <body>hello from homelab microvm</body>
+    </html>
+  '';
+}

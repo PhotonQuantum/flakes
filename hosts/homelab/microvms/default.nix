02f7d198df6c89a93de82b5717e0c677aca0e3a3
LightQuantum <self@lightquantum.me>
2026-02-20 03:00:22 -0500

    feat: isolate "isolated" vms from host and lan network

--- hosts/homelab/microvms/default.nix
+++ hosts/homelab/microvms/default.nix
@@ -20,17 +20,60 @@ let
     machine: "v ${builtins.dirOf machine.dataVolumeResolved.hostImagePath} 0770 microvm kvm - -"
   ) dataVolumeMachines;
 
-  natInternalInterfaces = lib.unique (
-    map (group: group.bridgeName) (builtins.filter (group: group.natEnabled) allGroupConfigs)
+  natEnabledGroups = builtins.filter (group: group.natEnabled) allGroupConfigs;
+  natInternalInterfaces = lib.unique (map (group: group.bridgeName) natEnabledGroups);
+  trustedInternalInterfaces = lib.unique (
+    map (group: group.bridgeName) (
+      builtins.filter (group: !(group.isolated or false)) natEnabledGroups
+    )
   );
+  internetOnlyInterfaces = lib.unique (
+    map (group: group.bridgeName) (
+      builtins.filter (group: (group.isolated or false) && group.natEnabled) allGroupConfigs
+    )
+  );
+
+  privateIPv4Cidrs = [
+    "10.0.0.0/8"
+    "172.16.0.0/12"
+    "192.168.0.0/16"
+    "169.254.0.0/16"
+    "100.64.0.0/10"
+  ];
+
+  isolatedInputCommands = lib.concatMapStrings (
+    bridgeName: ''
+      # Block isolated guests from reaching host services on any port.
+      iptables -w -I nixos-fw 1 -i '${bridgeName}' -j DROP
+      ip6tables -w -I nixos-fw 1 -i '${bridgeName}' -j DROP
+    ''
+  ) internetOnlyInterfaces;
+
+  mkPrivateForwardDropCommands =
+    bridgeName:
+    lib.concatMapStrings (
+      cidr: "iptables -w -I nixos-filter-forward 1 -i '${bridgeName}' -d '${cidr}' -j DROP\n"
+    ) (lib.reverseList privateIPv4Cidrs);
+
+  isolatedNatCommands = lib.concatMapStrings (
+    bridgeName: ''
+      # NAT module already adds accept rules for internal interfaces, so force
+      # internet-only behavior by inserting stricter rules at chain head.
+      iptables -w -I nixos-filter-forward 1 -i '${bridgeName}' -j DROP
+      iptables -w -I nixos-filter-forward 1 -i '${bridgeName}' -o '${homelabSecrets.uplinkName}' -j ACCEPT
+      ${mkPrivateForwardDropCommands bridgeName}
+    ''
+  ) internetOnlyInterfaces;
 in
 {
   imports = [ ./backup.nix ];
 
   systemd.tmpfiles.rules = dataVolumeSubvolumeTmpfiles;
 
-  # Allow forwarding/NAT for all declared MicroVM bridge groups.
-  networking.firewall.trustedInterfaces = natInternalInterfaces;
+  # Keep routed groups trusted for host access, but do not trust isolated groups.
+  networking.firewall.trustedInterfaces = trustedInternalInterfaces;
+  networking.firewall.extraCommands = isolatedInputCommands;
+  networking.nat.extraCommands = isolatedNatCommands;
 
   networking.nat = {
     enable = natInternalInterfaces != [ ];

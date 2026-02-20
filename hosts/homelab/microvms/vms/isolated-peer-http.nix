acd20bbca45bb74c40b5c6358d51b41b67800bd8
LightQuantum <self@lightquantum.me>
2026-02-20 04:04:57 -0500

    homelab: cleanup test vms

--- hosts/homelab/microvms/vms/isolated-peer-http.nix
+++
@@ -1,6 +0,0 @@
-import ./http-with-peer-check.nix {
-  title = "isolated peer microvm";
-  body = "hello from isolated-peer-http microvm";
-  serverName = "isolated-peer-http.local";
-  peerVmName = "experiment-http";
-}

9d267020f6009a9974feb0e369b26839b4a6c072
LightQuantum <self@lightquantum.me>
2026-02-25 23:38:46 -0500

    feat: forgejo oauth for every host

--- hosts/arch/home.nix
+++ hosts/arch/home.nix
@@ -90,15 +90,6 @@
       };
     };
 
-    programs.git.settings = {
-      "credential \"https://git.lightquantum.me\"" = {
-        helper = [ "cache --timeout 7200" "oauth" ];
-        oauthClientId = "a4792ccc-144e-407e-86c9-5e7d8d9c3269";
-        oauthAuthURL = "/login/oauth/authorize";
-        oauthTokenURL = "/login/oauth/access_token";
-      };
-    };
-
     # Do not include any package except explicitly listed.
     packageRestrictions = {
       enable = true;
--- profiles/home/modules/vcs/git.nix
+++ profiles/home/modules/vcs/git.nix
@@ -28,6 +28,17 @@ in
       absorb.maxStack = 50;
       merge.tool = "nvimdiff";
       core.autocrlf = "input";
+
+      "credential \"https://git.lightquantum.me\"" = {
+        helper = [ "cache --timeout 7200" "oauth" ];
+        oauthClientId = "a4792ccc-144e-407e-86c9-5e7d8d9c3269";
+        oauthAuthURL = "/login/oauth/authorize";
+        oauthTokenURL = "/login/oauth/access_token";
+      };
     };
   };
+
+  home.packages = with pkgs; [
+    (if configOnly then emptyDirectory else git-credential-oauth)
+  ];
 }

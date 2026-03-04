df2c426486f46f1ae51a1c2e675ad84efd7c38e8
LightQuantum <self@lightquantum.me>
2026-03-04 17:19:28 -0500

    homelab: v6 isolation

--- hosts/homelab/microvms/network/isolation.nix
+++ hosts/homelab/microvms/network/isolation.nix
@@ -19,29 +19,43 @@ let
     "100.64.0.0/10"
   ];
 
-  isolatedInputCommands = lib.concatMapStrings (
-    bridgeName: ''
-      # Block isolated guests from reaching host services on any port.
-      iptables -w -I nixos-fw 1 -i '${bridgeName}' -j DROP
-      ip6tables -w -I nixos-fw 1 -i '${bridgeName}' -j DROP
-    ''
-  ) internetOnlyInterfaces;
+  privateIPv6Cidrs = [
+    "fc00::/7"
+    "fe80::/10"
+    "ff00::/8"
+  ];
 
-  mkPrivateForwardDropCommands =
-    bridgeName:
-    lib.concatMapStrings (
-      cidr: "iptables -w -I nixos-filter-forward 1 -i '${bridgeName}' -d '${cidr}' -j DROP\n"
-    ) (lib.reverseList privateIPv4Cidrs);
+  isolatedInputCommands = lib.concatMapStrings (bridgeName: ''
+    # Block isolated guests from reaching host services on any port.
+    iptables -w -I nixos-fw 1 -i '${bridgeName}' -j DROP
+    ip6tables -w -I nixos-fw 1 -i '${bridgeName}' -j DROP
+  '') internetOnlyInterfaces;
 
-  isolatedNatCommands = lib.concatMapStrings (
-    bridgeName: ''
+  isolatedNatCommands =
+    let
+      mkPrivateForwardDropCommands =
+        bridgeName:
+        lib.concatMapStrings (
+          cidr: "iptables -w -I nixos-filter-forward 1 -i '${bridgeName}' -d '${cidr}' -j DROP\n"
+        ) (lib.reverseList privateIPv4Cidrs);
+      mkPrivateForwardDropCommands6 =
+        bridgeName:
+        lib.concatMapStrings (
+          cidr: "ip6tables -w -I nixos-filter-forward 1 -i '${bridgeName}' -d '${cidr}' -j DROP\n"
+        ) (lib.reverseList privateIPv6Cidrs);
+    in
+    lib.concatMapStrings (bridgeName: ''
       # NAT module already adds accept rules for internal interfaces, so force
       # internet-only behavior by inserting stricter rules at chain head.
       iptables -w -I nixos-filter-forward 1 -i '${bridgeName}' -j DROP
       iptables -w -I nixos-filter-forward 1 -i '${bridgeName}' -o '${homelabSecrets.uplinkName}' -j ACCEPT
       ${mkPrivateForwardDropCommands bridgeName}
-    ''
-  ) internetOnlyInterfaces;
+
+      # Mirror internet-only behavior for IPv6 forwarding as well.
+      ip6tables -w -I nixos-filter-forward 1 -i '${bridgeName}' -j DROP
+      ip6tables -w -I nixos-filter-forward 1 -i '${bridgeName}' -o '${homelabSecrets.uplinkName}' -j ACCEPT
+      ${mkPrivateForwardDropCommands6 bridgeName}
+    '') internetOnlyInterfaces;
 in
 {
   networking.firewall.extraCommands = isolatedInputCommands;

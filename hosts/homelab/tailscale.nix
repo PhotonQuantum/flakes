d34a0d67977542bc1fd65941dbfe2aa5a8269b4d
LightQuantum <self@lightquantum.me>
2026-03-05 01:34:26 -0500

    homelab: tailscale

---
+++ hosts/homelab/tailscale.nix
@@ -0,0 +1,38 @@
+{
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  registry = import ./microvms/registry.nix { inherit inputs; };
+  vmLib = import ./microvms/lib.nix { inherit lib; };
+  resolvedGroups = vmLib.resolveGroups registry.bridgeGroups;
+
+  advertisedRoutes = lib.unique (
+    map
+      (group: "${group.ipv4Prefix}.0/${toString group.cidr}")
+      (builtins.filter (group: group.networkPolicy.hostAccess) (builtins.attrValues resolvedGroups))
+  );
+in
+{
+  services.tailscale = {
+    enable = true;
+    authKeyFile = "/var/keys/tailscale_key";
+    openFirewall = true;
+    disableTaildrop = true;
+    useRoutingFeatures = "server";
+    extraSetFlags = lib.optionals (advertisedRoutes != [ ]) [
+      "--advertise-routes=${lib.concatStringsSep "," advertisedRoutes}"
+    ];
+  };
+  services.networkd-dispatcher = {
+    enable = true;
+    rules."50-tailscale-optimizations" = {
+      onState = [ "routable" ];
+      script = ''
+        ${pkgs.ethtool}/bin/ethtool -K lan0 rx-udp-gro-forwarding on rx-gro-list off
+      '';
+    };
+  };
+}

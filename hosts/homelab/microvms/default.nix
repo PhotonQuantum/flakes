e55245f2277daf0ed96bce1c86c17245ae32bef7
LightQuantum <self@lightquantum.me>
2026-03-05 21:10:11 -0500

    homelab: use vsock for remote journal

--- hosts/homelab/microvms/default.nix
+++ hosts/homelab/microvms/default.nix
@@ -1,4 +1,4 @@
-{ inputs, lib, ... }:
+{ inputs, lib, pkgs, ... }:
 let
   homelabSecrets = import ../../../secrets/homelab.nix;
   vmLib = import ./lib.nix { inherit lib; };
@@ -28,20 +28,29 @@ in
     })
   ];
 
-  systemd.tmpfiles.rules =
-    let
-      volumeRules = map (
-        machine: "v ${builtins.dirOf machine.dataVolumeResolved.hostImagePath} 0770 microvm kvm - -"
-      ) (builtins.filter (machine: machine.dataVolumeResolved != null) allMachineConfigs);
-      journalDirRules = map (
-        machine: "d /var/lib/microvms/${machine.name}/journal 0755 microvm kvm - -"
-      ) allMachineConfigs;
-      journalRules = map (
-        machine:
-        "L+ /var/log/journal/${machine.machineId} - - - - /var/lib/microvms/${machine.name}/journal/${machine.machineId}"
-      ) allMachineConfigs;
-    in
-    volumeRules ++ journalDirRules ++ journalRules;
+  systemd.tmpfiles.rules = map (
+    machine: "v ${builtins.dirOf machine.dataVolumeResolved.hostImagePath} 0770 microvm kvm - -"
+  ) (builtins.filter (machine: machine.dataVolumeResolved != null) allMachineConfigs);
+
+  services.journald.remote = {
+    enable = true;
+    listen = "http";
+    settings.Remote = {
+      SplitMode = "host";
+      MaxUse = "5G";
+      KeepFree = "10G";
+    };
+  };
+
+  systemd.services.systemd-journal-remote.serviceConfig.ExecStart = lib.mkForce [
+    ""
+    "${pkgs.systemd}/lib/systemd/systemd-journal-remote --output=/var/log/journal/remote/"
+  ];
+
+  systemd.sockets.systemd-journal-remote.listenStreams = lib.mkForce [
+    ""
+    "vsock::19534"
+  ];
 
   microvm = {
     autostart = builtins.attrNames resolvedMachines;

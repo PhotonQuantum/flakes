30ac7e569f1e5b11a67d2ca61476c5af3b678c0b
LightQuantum <self@lightquantum.me>
2026-05-03 03:11:56 -0400

    homelab: resolve new home domain

--- hosts/homelab/coredns.nix
+++ hosts/homelab/coredns.nix
@@ -6,6 +6,7 @@
   };
 
   environment.etc."coredns/db.home.arpa".source = ./coredns/db.home.arpa;
+  environment.etc."coredns/db.lqhome.me".source = ./coredns/db.lqhome.me;
 
   networking.firewall.interfaces.tailscale0 = {
     allowedUDPPorts = [ 53 ];
